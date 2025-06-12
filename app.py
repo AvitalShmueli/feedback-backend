8c00ac3b290a729fdd62f6d119fb3cadb93517a1
AvitalShmueli <AvitalShmueli@gmail.com>
2025-06-12 15:08:37 +0300

    fix app.py

--- app.py
+++ app.py
@@ -9,24 +9,25 @@ app = Flask(__name__)
 Swagger(app)
 
 # Ensure the logs folder exists
-os.makedirs("logs", exist_ok=True)
+#os.makedirs("logs", exist_ok=True)
 
 # Configure the logging system
 logging.basicConfig(
-    level=logging.DEBUG,  # or INFO, WARNING, ERROR
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler("logs/app.log"),  # <- logs will be in logs/app.log
-        logging.StreamHandler(),  # <- still prints to console too
-    ],
+    level=logging.INFO,  # or DEBUG,INFO, WARNING, ERROR
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    #handlers=[
+    #    logging.FileHandler("logs/app.log"),  # <- logs will be in logs/app.log
+    #    logging.StreamHandler(),  # <- still prints to console too
+    #],
 )
 
 # Reduce logs from external libraries
-logging.getLogger("pymongo").setLevel(logging.WARNING)
-logging.getLogger("werkzeug").setLevel(logging.WARNING)
+#logging.getLogger("pymongo").setLevel(logging.WARNING)
+#logging.getLogger("werkzeug").setLevel(logging.WARNING)
 
 # Create a logger object
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 # Initialize Database Connection
@@ -35,7 +36,7 @@ MongoConnectionHolder.initialize_db()
 # Import the routes
 initial_routes(app)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8088))
-    logger.info(f"Starting the server on port {port}...")
-    app.run(debug=True, host="0.0.0.0", port=port, use_reloader=False)
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 8088))
+#    logger.info(f"Starting the server on port {port}...")
+#    app.run(debug=True, host="0.0.0.0", port=port, use_reloader=False)
