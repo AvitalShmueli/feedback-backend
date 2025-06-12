dd99cbc83a62b43fae5db4bdd9f0dcf2eb52643b
AvitalShmueli <AvitalShmueli@gmail.com>
2025-06-12 15:44:18 +0300

    fix app.py

--- app.py
+++ app.py
@@ -8,27 +8,15 @@ import logging
 app = Flask(__name__)
 Swagger(app)
 
-# Ensure the logs folder exists
-#os.makedirs("logs", exist_ok=True)
-
 # Configure the logging system
 logging.basicConfig(
-    level=logging.INFO,  # or DEBUG,INFO, WARNING, ERROR
+    level=logging.INFO, # or DEBUG,INFO, WARNING, ERROR
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    #handlers=[
-    #    logging.FileHandler("logs/app.log"),  # <- logs will be in logs/app.log
-    #    logging.StreamHandler(),  # <- still prints to console too
-    #],
 )
-
-# Reduce logs from external libraries
-#logging.getLogger("pymongo").setLevel(logging.WARNING)
-#logging.getLogger("werkzeug").setLevel(logging.WARNING)
 
 # Create a logger object
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
 
 # Initialize Database Connection
 MongoConnectionHolder.initialize_db()
@@ -36,7 +24,7 @@ MongoConnectionHolder.initialize_db()
 # Import the routes
 initial_routes(app)
 
-#if __name__ == "__main__":
-#    port = int(os.environ.get("PORT", 8088))
-#    logger.info(f"Starting the server on port {port}...")
-#    app.run(debug=True, host="0.0.0.0", port=port, use_reloader=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8088))
+    logger.info(f"Starting the server on port {port}...")
+    app.run(debug=True, host="0.0.0.0", port=port, use_reloader=False)
